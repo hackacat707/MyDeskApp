85a39b8ddb79e769556874958776b1b5ef87dfa9
Rem <remigijavin@gmail.com>
2019-11-17 18:58:26 +0000

    small changes

--- startbootstrap-bare-gh-pages/databaseConnection.php
+++ startbootstrap-bare-gh-pages/databaseConnection.php
@@ -1,7 +1,8 @@
 <?php
+// Change this to your connection info.
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'test';
-
+$DATABASE_USER = 'id11438325_mydeskapp';
+$DATABASE_PASS = '5minecrafts@boxes!';
+$DATABASE_NAME = 'id11438325_mydeskapp';
+// Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
