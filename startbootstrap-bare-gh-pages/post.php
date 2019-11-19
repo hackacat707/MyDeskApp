1f2cad501d4d05359845f7981cb29cc89a8536f7
bellalhossain <38021850+bellalhossain@users.noreply.github.com>
2019-11-19 20:51:10 +0000

    Update post.php

--- startbootstrap-bare-gh-pages/post.php
+++ startbootstrap-bare-gh-pages/post.php
@@ -55,7 +55,7 @@ if(isset($_POST['submit'])) {
                     <a class="nav-link" href="register.html">Sign Up </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="register.html">Sign In</a>
+                    <a class="nav-link" href="login.html">Sign In</a>
                 </li>
 				<li class="nav-item">
                     <a class="nav-link" href="FAQ.html">FAQ</a>
