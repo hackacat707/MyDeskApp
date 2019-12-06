129afcac7f01b3f9359d8331d1f374af6f41da41
Conor Carroll <45122826+CC-94@users.noreply.github.com>
2019-12-06 15:25:59 +0000

    Added Geocode maps to post page

--- startbootstrap-bare-gh-pages/details.php
+++ startbootstrap-bare-gh-pages/details.php
@@ -21,6 +21,30 @@ include "vendor/bootstrap/css/AdditionalCSS.css";
 <html lang="en">
 
 <head>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 400px;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+      #floating-panel {
+        left: 25%;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
+      }
+    </style>
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -146,7 +170,43 @@ if (mysqli_num_rows ($result)>0){
       </div>
 	</div>
   </div>
+      <div id="floating-panel">
+      <input id="address" type="textbox" value="Enter Address">
+      <input id="submit" type="button" value="Find">
+    </div>
   
+   <div id="map"></div>
+    <script>
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: {lat:53.3498, lng: -6.2603}
+        });
+        var geocoder = new google.maps.Geocoder();
+
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfwJ2ruEMTpoByOJ75p3qkGchcqTNkhXM&callback=initMap">
+    </script>
   
 
   <!-- Bootstrap core JavaScript -->
