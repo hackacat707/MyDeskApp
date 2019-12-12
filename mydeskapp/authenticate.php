3943475c72b67272d04f996cb97467fbec602208
Ioana <ioanaavram46@yahoo.com>
2019-12-12 15:34:57 +0000

    Update authenticate.php

--- mydeskapp/authenticate.php
+++ mydeskapp/authenticate.php
@@ -2,13 +2,8 @@
 session_start();
 // Change this to your connection info.
 
-include "databaseConnection.php";
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'id11438325_mydeskapp';
-$DATABASE_PASS = '5minecrafts@boxes!';
-$DATABASE_NAME = 'id11438325_mydeskapp';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+include_once 'databaseConnection.php';
+
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
